81effd6994e428a5275abb728d6a51bb4ea4aab7
Javier-Cs <krchipullajavier@gmail.com>
2025-06-27 20:46:41 -0500

    implementacion de completacion de palabras

--- pruebaArrays/prueba2/Data/logica.cs
+++ pruebaArrays/prueba2/Data/logica.cs
@@ -9,13 +9,59 @@ namespace pruebaArrays.prueba2.Data
     public class Logica
     {
         string[] palabras = {"linda", "camila", "luciana",
-            "beatriz", "aleja", "mariana", "melisa", "carla", "andreina" };
+            "beatriz", "alejandra", "mariana", "melisa", "catalina", "andreina" };
+        string palabra;
+        string palabraABuscar;
+
 
         public void juego()
         {
-            string palabraABuscar = obtenerPalabraRandom();
+            palabra = obtenerPalabraRandom();
+
+            palabraABuscar = palabra;
             char[] arrayPalabra = palabraABuscar.ToCharArray();
+
+            palabraABuscar = palabraABuscar.Replace("a", "_");
+            palabraABuscar = palabraABuscar.Replace("l", "_");
+            palabraABuscar = palabraABuscar.Replace("r", "_");
+
+            char[] arrayBuscar = palabraABuscar.ToCharArray();
+
+            Console.WriteLine("");
+            Console.WriteLine("El nombre a buscar es:");
+            Console.WriteLine(palabraABuscar);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("");
+                Console.Write("Ingrese una caracter: ");
+                char caracter = Console.ReadKey().KeyChar;
+
+                for (int j = 0; j < arrayPalabra.Length; j++)
+                {
+                    if (arrayPalabra[j] == caracter)
+                    {
+                        arrayBuscar[j] = caracter;
+                    }
+
+                }
+                
+            }
+            for (int b = 0; b < arrayPalabra.Length; b++)
+            {
+                Console.Write(arrayPalabra[b]);
+            }
+            Console.WriteLine("");
+            for (int b = 0; b < arrayPalabra.Length; b++)
+            {
+                Console.Write(arrayBuscar[b]);
+            }
+
         }
+            
+
+            
+       
 
 
         private string obtenerPalabraRandom() {
@@ -23,6 +69,14 @@ namespace pruebaArrays.prueba2.Data
             int indi = random.Next(palabras.Length);
             return palabras[indi];
         }
+
+        //private void convertirCaracter(char a) {
+        //    if () 
+        //    {
+        //    }
+        //}
+
+
 
         
 
