15c63478a76281dff40bb3601e547d882c145fa6
Javier-Cs <krchipullajavier@gmail.com>
2025-07-01 15:09:53 -0500

    Uso de herencia - Implementación de sobre escritura con virtual y override en los métodos virtual en el método padre, opcionalmente puedes usar protected override en el método hijo - diferencia entre heredar clases y heredar interfaces la clase solo se la puede implementar 1 ves la interfaz se la puede implementar múltiples veces pero de sebe de seguir su contacto.

--- pruebaArrays/Program.cs
+++ pruebaArrays/Program.cs
@@ -1,26 +1,75 @@
-﻿using pruebaArrays.prueba1.Menu;
-using pruebaArrays.prueba3.logica;
-using pruebaArrays.prueba4.Menu;
+﻿//using pruebaArrays.prueba1.Menu;
+//using pruebaArrays.prueba3.logica;
+//using pruebaArrays.prueba4.Menu;
 
-namespace pruebaArrays
+//namespace pruebaArrays
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //MenuTer menu = new MenuTer();
+//            //menu.menuInporta();
+//            //MenuImpl menu = new MenuImpl();
+//            //menu.presentarM();
+
+//            //Menu03 menu = new Menu03();
+//            //menu.mostrarMenu();
+//            //MostrarMenu menu = new MostrarMenu();
+//            //menu.implementaMenu();
+
+//            Console.ReadKey();
+//        }
+
+
+//    }
+//}
+
+
+
+//var sale = new Sale(17);
+
+
+
+//class Sale { 
+//    public decimal total {  get; set; }
+
+//    public Sale(decimal total) {
+//        total = total;
+//    } 
+
+//    public virtual string GetInfo(){
+//        return "total es: " + total;
+//    }
+//}
+
+
+//// herencia 
+//class SaleWithTax : Sale
+//{
+//    public decimal Tax { get; set; }
+//    public SaleWithTax(decimal total, decimal tax) : base(total)
+//    {
+//        Tax = tax;
+//    }
+
+//    public override string GetInfo() {
+//        return "El total con tax es: " + total;
+//    }
+
+
+//}
+
+public interface ISale { 
+    public decimal total { get; set; }
+}
+
+public interface ISave {
+    public void Save() { }
+}
+
+public class Sale : ISale, ISave 
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //MenuTer menu = new MenuTer();
-            //menu.menuInporta();
-            //MenuImpl menu = new MenuImpl();
-            //menu.presentarM();
+    public decimal total { get; set; }
 
-            //Menu03 menu = new Menu03();
-            //menu.mostrarMenu();
-            MostrarMenu menu = new MostrarMenu();
-            menu.implementaMenu();
-
-            Console.ReadKey();
-        }
-
-       
-    }
 }
