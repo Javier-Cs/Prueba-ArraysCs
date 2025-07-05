8be38ff96ac0451032a56ebc81508f1acbeb4747
Javier-Cs <krchipullajavier@gmail.com>
2025-07-04 19:43:36 -0500

    Uso de diccionarios de clave, valor

---
+++ PruebaListas/Diccionario/UsoDeDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaListas.Diccionario
+{
+    public class UsoDeDictionary
+    {
+        Dictionary<int, string> catalogoDeProductos = new();
+        public void Mostrar() { 
+            Console.WriteLine("Ejercicio 1: Catálogo de Productos (Dictionary<TKey, TValue>)");
+        
+            // agregacion de algunos productos
+            catalogoDeProductos.Add(101, "Laptop");
+            catalogoDeProductos.Add(102, "Teclado");
+            catalogoDeProductos.Add(103, "Monitor");
+            catalogoDeProductos.Add(104, "Mouse");
+
+            Console.WriteLine("Productos por catalogo:");
+            foreach (var producto in catalogoDeProductos)
+            {
+                Console.WriteLine($"ID: {producto.Key}, Nombre: {producto.Value}");
+            }
+        }
+
+        public void BuscarProductosById(int id) {
+            if (catalogoDeProductos.TryGetValue(id, out string nombreProd))
+            {
+                Console.WriteLine($"\nProducto con ID {id}: {nombreProd}");
+            }
+            else
+            {
+                Console.WriteLine($"\nProducto con ID: {id} no fue encontrado");
+            }
+
+        }
+
+        public void ConprobarSiProductExiste(string producto) {
+            if (catalogoDeProductos.ContainsValue(producto))
+            {
+                Console.WriteLine($"\nEl catálogo contiene el producto '{producto}'.");
+            }
+            else
+            {
+                Console.WriteLine($"\nEl catálogo NO contiene el producto '{producto}'.");
+            }
+        }
+        
+    }
+}
--- PruebaListas/Program.cs
+++ PruebaListas/Program.cs
@@ -1,15 +1,22 @@
-﻿using PruebaListas.Modelo;
+﻿using PruebaListas.Diccionario;
+using PruebaListas.Modelo;
 
 namespace PruebaListas
 {
     internal class Program
     {
         static void Main(string[] args) { 
-            Juego juego = new Juego();
-            juego.ordenarDeMayoAMenor();
+
+            UsoDeDictionary dyctionary = new UsoDeDictionary();
+            dyctionary.Mostrar();
+            dyctionary.BuscarProductosById(12);
+            dyctionary.ConprobarSiProductExiste("Monitor");
 
 
 
+
+            //Juego juego = new Juego();
+            //juego.ordenarDeMayoAMenor();
 
         //    Menu menu = new Menu();
         //    menu.guardar();
@@ -21,24 +28,24 @@ namespace PruebaListas
 
         }
     }
-    public class Juego { 
-        List<int> puntajes = new List<int> {21, 54, 2 , 34, 24, 17, 66 };
+    //public class Juego { 
+    //    List<int> puntajes = new List<int> {21, 54, 2 , 34, 24, 17, 66 };
 
 
-        public void ordenarDeMayoAMenor() {
-            Console.WriteLine("Puntuaciones originales: "+ string.Join(",", puntajes));
+    //    public void ordenarDeMayoAMenor() {
+    //        Console.WriteLine("Puntuaciones originales: "+ string.Join(",", puntajes));
 
-            puntajes.Sort();
-            puntajes.Reverse();
-            Console.WriteLine("Puntuacion ordenada: "+ string.Join(",", puntajes));
-
-
-            int valormax = puntajes.Max();
-            Console.WriteLine("valor maximo de la lista: "+valormax);
-            int valorMin = puntajes.Min();
-            Console.WriteLine("valor minimo de la lista: "+valorMin);
-        }
+    //        puntajes.Sort();
+    //        puntajes.Reverse();
+    //        Console.WriteLine("Puntuacion ordenada: "+ string.Join(",", puntajes));
 
 
-    }
+    //        int valormax = puntajes.Max();
+    //        Console.WriteLine("valor maximo de la lista: "+valormax);
+    //        int valorMin = puntajes.Min();
+    //        Console.WriteLine("valor minimo de la lista: "+valorMin);
+    //    }
+
+
+    //}
 }
