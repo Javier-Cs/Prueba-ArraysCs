f4e0e767d11d28d16cbddd655426e5327d7117a7
Javier-Cs <krchipullajavier@gmail.com>
2025-07-07 14:12:42 -0500

    implementacion de hashSet el cual no acepta valores duplicados y esta en desorden. uso de .Count() para obtener la cantidad de elementos guardadoos

---
+++ ConhashSet/Clases/UsoHashSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConhashSet.Clases
+{
+    public class UsoHashSet
+    {
+        public void mostrar() {
+            Console.WriteLine("Ejercicio 1 de prueba de usuarios");
+
+            // creacion de HashSet de strings
+            HashSet<string> usuariosConectados = new HashSet<string>();
+
+            Console.WriteLine($"añadiendo 'usuario 1': {usuariosConectados.Add("user1")}");
+            Console.WriteLine($"añadiendo 'usuario 2': {usuariosConectados.Add("user2")}");
+            Console.WriteLine($"añadiendo 'usuario 3': {usuariosConectados.Add("user3")}");
+
+            Console.WriteLine($"añadiendo 'usuario 1': {usuariosConectados.Add("user1")}");
+
+
+
+
+            Console.WriteLine("\nUsuarios conectados" );
+            foreach (var item in usuariosConectados)
+            {
+                Console.WriteLine($"item");
+            }
+
+            Console.WriteLine($"Número total de usuarios únicos: {usuariosConectados.Count}");
+
+            // Comprobar si un usuario está conectado
+            string usuarioChecar = "user2";
+            Console.WriteLine($"\n¿'user2' está conectado? {usuariosConectados.Contains(usuarioChecar)}");
+            usuarioChecar = "user3";
+            Console.WriteLine($"¿'user3' está conectado? {usuariosConectados.Contains(usuarioChecar)}");
+        }
+    }
+}
---
+++ ConhashSet/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConhashSet.Clases;
+
+namespace ConhashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           UsoHashSet hashset = new UsoHashSet();
+            hashset.mostrar();
+        }
+    }
+}
