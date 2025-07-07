1cd24e48746813004ecfe3f75969ce6a1cbb120b
Javier-Cs <krchipullajavier@gmail.com>
2025-07-07 15:07:45 -0500

    uso de Queue y el ingreso y eliminacion de datos mediante - Enqueue y Dequeue

--- ConhashSet/Clases/UsoHashSet.cs
+++ ConhashSet/Clases/UsoHashSet.cs
@@ -9,32 +9,28 @@ namespace ConhashSet.Clases
     public class UsoHashSet
     {
         public void mostrar() {
-            Console.WriteLine("Ejercicio 2 hashSet");
+            Console.WriteLine("Ejercicio de Queue");
+            Queue<int> listaDePedidos = new Queue<int>();
+
+            // añadir a la cola
+            listaDePedidos.Enqueue(1001);
+            listaDePedidos.Enqueue(1002);
+            listaDePedidos.Enqueue(1003);
+            listaDePedidos.Enqueue(1004);
 
 
-            List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
-            List<int> list2 = new List<int> { 4, 5, 6, 7, 8 };
-
-            HashSet<int> listaNum1 = new HashSet<int> (list1);
-            HashSet<int> listaNum2 = new HashSet<int> (list2);
+            //impresion
+            Console.WriteLine("Pedidos en cola: "+string.Join(",", listaDePedidos));
+            Console.WriteLine($"Pedidos pendientes: {listaDePedidos.Count}");
 
 
-            Console.WriteLine("Lista A: "+ string.Join(",", listaNum1));
-            Console.WriteLine("Lista B: " + string.Join(",", listaNum2));
-
-            listaNum1.IntersectWith(listaNum2);
-            Console.WriteLine("\nElementos en común (intersección): " + string.Join(", ", listaNum1));
-
-
-            listaNum1 = new HashSet<int>(listaNum2);
-            listaNum1.UnionWith(listaNum2);
-
-            Console.WriteLine("Todos los elementos únicos (unión): " + string.Join(", ", listaNum1));
-
+            Console.WriteLine("\nProcesando pedidos:");
+            while (listaDePedidos.Count  > 0) {
+                int pedidoActual = listaDePedidos.Dequeue();
+                Console.WriteLine($"Procesando pedidos: {pedidoActual}. pedidos restantes: {listaDePedidos.Count}");
+            }
+            Console.WriteLine("Cola de pedidos vacía.");
             Console.WriteLine("-------------------------------------------------");
-
-
-
         }
     }
 }
