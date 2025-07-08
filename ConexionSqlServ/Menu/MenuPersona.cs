c9bd4fb7b888362b0d105a5175edba3ebb949077
Javier-Cs <krchipullajavier@gmail.com>
2025-07-08 15:55:21 -0500

    implementacion de menu y validació para los enteros

---
+++ ConexionSqlServ/Menu/MenuPersona.cs
@@ -0,0 +1,63 @@
+﻿using ConexionSqlServ.Validacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionSqlServ.Menu
+{
+    public class MenuPersona
+    {
+
+        public Validaciones validaciones;
+
+        public MenuPersona(Validaciones validaciones)
+        {
+            this.validaciones = validaciones;
+        }
+
+
+        public void mostrarMenu() {
+            bool loop = false;
+            int opcion = 0;
+            do {
+                opcion = menu();
+                switch (opcion) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        loop = true;
+                        break;
+                    default:
+                        loop = false;
+                        break;            
+                }
+            } while (!loop);
+
+            
+        }
+
+
+
+
+        private int menu() {
+            Console.WriteLine("\nSelecciona una opción:");
+            Console.WriteLine("1. Crear Persona");
+            Console.WriteLine("2. Ver Todas las Personas");
+            Console.WriteLine("3. Ver Persona por ID");
+            Console.WriteLine("4. Actualizar Persona");
+            Console.WriteLine("5. Eliminar Persona");
+            Console.WriteLine("6. Salir");
+            return validaciones.obtenerNumero("Escoja una Opcion: ");
+        }
+    }
+}
---
+++ ConexionSqlServ/Validacion/Validaciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionSqlServ.Validacion
+{
+    public class Validaciones
+    {
+
+        private bool EsNumero(string valor) {
+            return int.TryParse(valor, out _);
+        }
+
+        public int obtenerNumero(string message) {
+            string valor;
+            bool loop = false;
+
+
+            do {
+                Console.Write(message);
+                valor = Console.ReadLine();
+                if (!EsNumero(valor))
+                {
+                    Console.WriteLine("El valor ingresado es invalido...");
+                    loop = false;
+                }
+                else
+                {
+                    loop = true;
+                }
+            } while (!loop);
+            return Convert.ToInt32(valor);
+        }
+
+
+    }
+}
