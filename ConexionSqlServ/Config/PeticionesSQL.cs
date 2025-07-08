a21ee63b4964fc1930df79ae822e126072e2b57b
Javier-Cs <krchipullajavier@gmail.com>
2025-07-08 13:51:48 -0500

    Creamos una clase de peticiones SQL a la tabla - Implementación de los métodos crear, listar y eliminar - uso de using, - uso de SqlConnection para establecer las credenciales de la base de datos - utilización de SqlCommand en la que establecemos la petición (query) y la conexión

--- ConexionSqlServ/Config/PeticionesSQL.cs
+++ ConexionSqlServ/Config/PeticionesSQL.cs
@@ -8,10 +8,10 @@ namespace ConexionSqlServ.Config
 {
     public static class PeticionesSQL
     {
-        public static string queriSave = "INSERT INTO conect.Persona_tbl (nombrePersona, apellidoPersona, edad) VALUES (@nombre, @apellido, @edad)";
-        public static string querySelect = "SELECT idPersona, nombrePersona, apellidoPersona, edad FROM conect.Persona_tbl";
-        public static string queriObtenById = "SELECT idPersona, nombrePersona, apellidoPersona, edad FROM conect.Persona_tbl WHERE idPersona = @id";
-        public static string queriUpdate = "UPDATE conect.Persona_tbl SET nombrePersona = @nombre, apellidoPersona = @apellido, edad = @edad WHERE idPersona = @id";
-        public static string queridelete = "DELETE FROM conect.Persona_tbl WHERE idPersona = @id";
+        public static string Query_SavePersona = "INSERT INTO conect.Persona_tbl (nombrePersona, apellidoPersona, edad) VALUES (@nombre, @apellido, @edad)";
+        public static string Query_SelectPersona = "SELECT idPersona, nombrePersona, apellidoPersona, edad FROM conect.Persona_tbl";
+        public static string Query_ObtenByIdPersona = "SELECT idPersona, nombrePersona, apellidoPersona, edad FROM conect.Persona_tbl WHERE idPersona = @id";
+        public static string Query_UpdatePersona = "UPDATE conect.Persona_tbl SET nombrePersona = @nombre, apellidoPersona = @apellido, edad = @edad WHERE idPersona = @id";
+        public static string Query_DeletePersona = "DELETE FROM conect.Persona_tbl WHERE idPersona = @id";
     }
 }
--- ConexionSqlServ/Repository/PersonaRepository.cs
+++ ConexionSqlServ/Repository/PersonaRepository.cs
@@ -1,5 +1,7 @@
-﻿using ConexionSqlServ.Entity;
+﻿using ConexionSqlServ.Config;
+using ConexionSqlServ.Entity;
 using ConexionSqlServ.Service;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,36 +14,112 @@ namespace ConexionSqlServ.Repository
     public class PersonaRepository : PersonaService
     {
 
-        private readonly string _conexioSqlServer = "Data Source=JAVIERCS;" +
-            " Initial Catalog=conect.Persona_tbl;" +
-            "Integrated Security=True;";
+        private readonly string _conexioSqlServer = "Data Source=JAVIERCS; Initial Catalog=conect.Persona_tbl; Integrated Security=True";
 
         //constructor
         public PersonaRepository() { }
 
+        //------------------------------------------------------------------------------------------
+        public void InsertarPersona(PersonaEntity persona)
+        {
+            using (SqlConnection conexion = new SqlConnection(_conexioSqlServer)) {
+                //esta peticion se la puede obtener y almacenar en una variable
+                // o instanciarla directamente.
+                //PeticionesSQL.Query_SavePersona;
+                using(SqlCommand command = new SqlCommand(PeticionesSQL.Query_SavePersona, conexion)) {
+
+                    command.Parameters.AddWithValue("@nombre", persona.NombrePersona);
+                    command.Parameters.AddWithValue("@apellido", persona.ApellidoPersona);
+                    command.Parameters.AddWithValue("@edad", persona.Edad);
+
+                    try
+                    {
+                        conexion.Open();
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Persona guardada exitosamente");
+                    }
+                    catch (SqlException ex) {
+                        Console.WriteLine($"Error al guardar una persona {ex.Message}");
+                    }
+                    conexion.Close();
+                }
+            }
+        }
+
+
+        //------------------------------------------------------------------------------------------
+        public List<PersonaEntity> ObtenerAllPersonas()
+        {
+            List<PersonaEntity> ListaPersonas = new List<PersonaEntity>();
+
+            using (SqlConnection conexion = new SqlConnection(_conexioSqlServer)) {
+                using (SqlCommand command = new SqlCommand(PeticionesSQL.Query_SelectPersona, conexion)) {
+                    try
+                    {
+                        conexion.Open();
+                        using (SqlDataReader reader = command.ExecuteReader()) {
+                            while (reader.Read())
+                            {
+                                PersonaEntity persona = new PersonaEntity
+                                {
+                                    IdPersona = reader.GetInt32(reader.GetOrdinal("idPersona")),
+                                    NombrePersona = reader.GetString(reader.GetOrdinal("nombrePersona")),
+                                    ApellidoPersona = reader.GetString(reader.GetOrdinal("apellidoPersona")),
+                                    Edad = reader.GetInt32(reader.GetOrdinal("edad"))
+                                };
+                                ListaPersonas.Add(persona);
+                            }
+                        }
+                    }
+                    catch (SqlException ex) {
+                        Console.WriteLine($"No se pudo obtener la lista de personas${ex.Message}");
+                    }
+                }
+                conexion.Close();
+            }
+            return ListaPersonas;
+        }
+
+
+        //------------------------------------------------------------------------------------------
         public void ActualizarPersona(PersonaEntity persona)
         {
             throw new NotImplementedException();
         }
 
-        public void EliminarPersona(int id)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void InsertarPersona(PersonaEntity persona)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<PersonaEntity> ObtenerAllPersonas()
-        {
-            throw new NotImplementedException();
-        }
-
+        //------------------------------------------------------------------------------------------
         public PersonaEntity ObtenerPersonaByID(int id)
         {
             throw new NotImplementedException();
         }
+
+
+        //------------------------------------------------------------------------------------------
+        public void EliminarPersona(int id)
+        {
+            using (SqlConnection conexion = new SqlConnection(_conexioSqlServer)) {
+                using (SqlCommand command = new SqlCommand(PeticionesSQL.Query_DeletePersona, conexion)) {
+                    command.Parameters.AddWithValue("@id", id);
+                    try {
+                        conexion.Open();
+                        int rowsAffect = command.ExecuteNonQuery();
+                        if (rowsAffect > 0) {
+                            Console.WriteLine("Persona eliminada exitosamente.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró la persona con el ID especificado para eliminar.");
+                        }
+                    }
+                    catch (SqlException ex) {
+                        Console.WriteLine($"Error al eliminar persona: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
