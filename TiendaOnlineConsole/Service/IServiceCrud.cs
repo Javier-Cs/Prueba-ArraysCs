ab5a3f1a0edcaa9757711c9d31e9b25a91d2c8d3
Javier-Cs <krchipullajavier@gmail.com>
2025-07-09 21:04:18 -0500

    implementación de una interfaz generica

---
+++ TiendaOnlineConsole/Service/IServiceCrud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnlineConsole.Service
+{
+    // Introducimos <TEntity>, donde TEntity es un parámetro de tipo.
+    // Esto significa que cuando implementes esta interfaz, especificarás el tipo concreto
+    // (por ejemplo, PersonaEntity, ProductoEntity, etc.) que TEntity representará.
+    public interface IServiceCrud<TEntity> where TEntity : class
+    {
+        void Insertar(TEntity entity);
+        List<TEntity> ObtenerTodo();
+        TEntity ObtenerPorId(int id);
+        void Actualizar(TEntity entity);
+        void Eliminar(int id);
+    }
+}
