6bcec2e0da0eb135bb4396d6ffc487084db65201
Javier-Cs <krchipullajavier@gmail.com>
2025-07-14 17:43:54 -0500

    implementación de una nueva interfaz para detalle de pedido - detalle repository

--- TiendaOnlineConsole/Repository/DetallePedidoRepository.cs
+++ TiendaOnlineConsole/Repository/DetallePedidoRepository.cs
@@ -1,26 +1,56 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TiendaOnlineConsole.Config;
 using TiendaOnlineConsole.Entity;
 using TiendaOnlineConsole.Service;
 
 namespace TiendaOnlineConsole.Repository
 {
-    public class DetallePedidoRepository
+    public class DetallePedidoRepository : DetallePedidoService
     {
         private readonly string conectar = "Data Source=JAVIERCS;Initial Catalog=Tiendaonline_DB; User ID=sa; Password=21427711; TrustServerCertificate=True;";
-        private readonly PedidoRepository pedidoRepository;
+        private readonly ProductoRepository productoRepository;
 
-        public DetallePedidoRepository(PedidoRepository pedidoRepository)
+        public DetallePedidoRepository(ProductoRepository productoRepository)
         {
-            this.pedidoRepository = pedidoRepository;
+            this.productoRepository = productoRepository;
         }
 
-        public void InsertarDetallePedido(DetallePedidoEntity detalle) { }
-        public List<DetallePedidoEntity> ObtenerDetallesPorPedidoId(int pedidoId) {
-            return null;
+        public void InsertarDetallePedido(DetallePedidoEntity detalle)
+        {
+            using (SqlConnection conexion = new SqlConnection(conectar))
+            {
+                using (SqlCommand command = new SqlCommand(ConsultasSQL.DetallePedido_Insert, conexion))
+                {
+                    command.Parameters.AddWithValue("@PedidoId", detalle.id_Pedido);
+                    command.Parameters.AddWithValue("@ProductoId", detalle.id_Producto);
+                    command.Parameters.AddWithValue("@Cantidad", detalle.cantidad);
+                    command.Parameters.AddWithValue("@PrecioUnitario", detalle.precioUnitario);
+
+                    try {
+                        conexion.Open();
+                        command.ExecuteNonQuery();
+                        ProductoEntity producto = productoRepository.ObtenerPorId(detalle.id_Producto);
+
+                        if (producto != null)
+                        {
+                            productoRepository.ActualizarstockProductos(detalle.id_Producto, producto.stockProducto - detalle.cantidad);
+                        }
+                        Console.WriteLine($"  Detalle de pedido para Producto ID {detalle.id_Producto} agregado exitosamente. Stock actualizado.");
+                    } catch (SqlException ex) {
+                        Console.WriteLine($"Error al insertar detalle de pedido: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        public List<DetallePedidoEntity> ObtenerDetallesPorPedidoId(int pedidoId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
